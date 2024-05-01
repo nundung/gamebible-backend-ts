64753ba1c4bbcf2db7043cc755078f6ee6f78d10
nundung <jjy809123@naver.com>
2024-05-01 20:51:20 +0900

    Feat: validator 미들웨어 생성

---
+++ src/middleware/validator.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
+export { handleValidationErrors };
