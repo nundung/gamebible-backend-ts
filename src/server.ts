d444edbca5b40edadbc965f4c43323302981a365
nundung <jjy809123@naver.com>
2024-05-01 22:06:41 +0900

    Feat: 에러 핸들러 생성

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
-import express from 'express';
-import { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
@@ -10,6 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    res.status(err.status || 500).send({
+        message: err.status ? err.message : '예상하지 못한 에러가 발생했습니다.',
+    });
+});
 
 app.listen(process.env.HTTP_PORT, () => {
     console.log(`${process.env.HTTP_PORT}번 포트번호 서버실행`);
