df29fb41c318b5a3a331ed7e0bb14a71769fc254
nundung <jjy809123@naver.com>
2024-05-01 17:15:39 +0900

    Feat: postgres.ts 문서 생성

---
+++ src/config/postges.ts
@@ -0,0 +1,16 @@
+import { Pool, PoolConfig } from 'pg';
+require('dotenv').config();
+
+const psqlDBconfig = {
+    host: process.env.PSQL_HOST || '',
+    port: parseInt(process.env.PSQL_PORT || '5432'),
+    database: process.env.PSQL_DATABASE || '',
+    user: process.env.PSQL_USER || '',
+    password: process.env.PSQL_PW || '',
+    idleTimeoutMillis: 10 * 1000,
+    connectionTimeoutMillis: 15 * 1000,
+};
+
+const pool = new Pool(psqlDBconfig);
+
+export default pool;
