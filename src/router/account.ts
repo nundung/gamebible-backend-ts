17a509637841c2411fbf5d5cb14473d50b6c000d
nundung <jjy809123@naver.com>
2024-05-02 16:15:36 +0900

    Refacor: 로그인 API 수정

--- src/router/account.ts
+++ src/router/account.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { body } from 'express-validator';
 import { handleValidationErrors } from '../middleware/validator';
+import NotFoundException from '../exception/notFoundException';
 import jwt from 'jsonwebtoken';
 import pool from '../config/postges';
 import bcrypt from 'bcrypt';
@@ -26,10 +27,17 @@ router.post(
         try {
             // 사용자 정보 조회 (비밀번호는 해시된 상태로 저장되어 있음)
             const values = [id];
-            const { rows: userRows } = await pool.query(
+            // 제네릭: 타입을 동적으로 할당함. 그런 제네릭을 써야할 때가 있음
+            const { rows: userRows } = await pool.query<{
+                pw: string;
+                userIdx: number;
+                isAdmin: boolean;
+            }>(
                 `
                 SELECT
-                    *
+                    pw,
+                    user_idx AS "userIdx",
+                    is_admin AS "isAdmin"
                 FROM
                     account_local al
                 JOIN
@@ -38,31 +46,29 @@ router.post(
                     al.id = $1 AND u.deleted_at IS NULL`,
                 values
             );
-
-            if (userRows.length === 0) {
-                return res.status(204).send({ message: '로그인 실패' });
-            }
-            // 제네릭: 타입을 동적으로 할당함. 그런 제네릭을 써야할 때가 있음
             const user = userRows[0];
+
+            if (!user) {
+                throw new NotFoundException('해당 id로 가입된 사용자 존재하지 않음');
+            }
             // bcrypt.compare 함수로 비밀번호 비교
-            const match: boolean = await bcrypt.compare(pw, user.pw);
+            const match = await bcrypt.compare(pw, user.pw);
 
             if (!match) {
-                return res.status(401).send({ message: '비밀번호 일치하지 않음' });
+                throw new NotFoundException('비밀번호 일치하지 않음');
             }
 
             // 비밀번호가 일치하면 토큰 생성
             const token = jwt.sign(
                 {
-                    userIdx: user.user_idx,
-                    isAdmin: user.is_admin,
+                    userIdx: user.userIdx,
+                    isAdmin: user.isAdmin,
                 },
                 process.env.SECRET_KEY || '',
                 {
                     expiresIn: '5h',
                 }
             );
-
             res.status(200).send({ kakaoLogin: false, token: token, data: user });
         } catch (e) {
             next(e);
