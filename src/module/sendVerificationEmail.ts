2ac4ebf432390fc9c46d57431a6642bdf96fe28e
nundung <jjy809123@naver.com>
2024-05-04 19:25:16 +0900

    Fix: sendVerificationEmail 모듈 수정

--- src/module/sendVerificationEmail.ts
+++ src/module/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-async function sendVerificationEmail(email: string, code: string) {
+function sendVerificationEmail(email: string, code: string) {
     let transporter = nodemailer.createTransport({
         service: 'naver',
         auth: {
@@ -16,9 +16,9 @@ async function sendVerificationEmail(email: string, code: string) {
         text: `귀하의 인증 코드는 ${code} 입니다.`,
     };
 
-    await transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('이메일 전송 실패: ', error);
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error('이메일 전송 실패: ', err);
         } else {
             console.log('이메일 전송 성공: ' + info.response);
         }
