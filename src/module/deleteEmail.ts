bc8ed948229bde1cfa10bc6dd3fb0a48b0c11053
nundung <jjy809123@naver.com>
2024-05-04 15:46:43 +0900

    Fix: deleteEmail.ts 수정

--- src/module/deleteEmail.ts
+++ src/module/deleteEmail.ts
@@ -2,14 +2,16 @@ import pool from '../config/postgres';
 
 async function deleteExpiredCodes(pool) {
     try {
-        const deleteQuery = `
-            DELETE FROM email_verification
-            WHERE created_at < NOW() - INTERVAL '5 minutes'
-        `;
-        const result = await pool.query(deleteQuery);
-        console.log(`${result.rowCount}개 code 지워짐`);
-    } catch (error) {
-        console.error('Error deleting expired records:', error);
+        const { rowCount: DeleteCount } = await pool.query(
+            `DELETE
+            FROM
+                email_verification
+            WHERE
+                created_at < NOW() - INTERVAL '5 minutes'`
+        );
+        console.log(`${DeleteCount}개 code 지워짐`);
+    } catch (err) {
+        console.error('Error deleting expired records:', err);
     }
 }
 
