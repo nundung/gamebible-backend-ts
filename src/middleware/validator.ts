56fe8939e86be766dea582df99f0e6f36d81da93
nundung <jjy809123@naver.com>
2024-05-05 18:53:31 +0900

    Fix: vlaidator 모듈 안 쓰는 함수 제거

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 
 const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
